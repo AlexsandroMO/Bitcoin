e19498660d5e3a9fdaee7fdb17e9a5464ebdac8d
Alexsandro <sandrobass@hotmail.com>
2019-12-05 16:48:48 +0000

    Start project

---
+++ coin/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+# pip install django-crispy-forms
+
+
+'''Upload documents on Github
+
+git clone <nome>
+
+<entra na pasta criada>
+
+git add .
+
+git commit -m "texto"
+
+git push
+'''
